570ca96a3e3819a8b16aaa3a79392b487d7cc2e9
Jfp46 <101796830+Jfp46@users.noreply.github.com>
2022-04-10 13:30:43 +0200

    Add files via upload

---
+++ Hola_mundo.cpp
@@ -0,0 +1,19 @@
+// Comentarios del programa
+// Estructura inicial para c++
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+#define N_ventana "Producciones JFP" // Defino el nombre de la ventana
+
+int main () {
+
+    SetConsoleTitle(N_ventana); //Cambia el nombre de la ventana
+    
+	cout << "\n Hola mundo \n" << endl;
+
+	system ("pause");
+	return 0;
+}
+
+
